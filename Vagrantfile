7925becbe7fe01cc70b76cd379c4c46355c580ee
ffa7 <ffa7@sfu.ca>
2016-03-17 21:11:40 -0700

    Adding nginx and unicorn configuration, and sessions controller setup for login

--- Vagrantfile
+++ Vagrantfile
@@ -10,6 +10,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   config.vm.network "forwarded_port", guest: 80, host: 8080
+  #config.vm.network "forwarded_port", guest: 3000, host: 3000
+
 
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
@@ -23,7 +25,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # If you need to more than double the defaults for this course, you have
   # done something wrong.
   cpus = "1"
-  memory = "512" # MB
+  memory = "1024" # MB
   config.vm.provider :virtualbox do |vb|
     vb.customize ["modifyvm", :id, "--cpus", cpus, "--memory", memory]
   end
--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -15,3 +15,63 @@ end
 execute 'ntp_restart' do
   command 'service ntp restart'
 end
+
+#installing web server nginx
+package "nginx"
+#install and set up postgres database
+package "postgresql"
+package "libpq-dev"
+execute 'echo "CREATE DATABASE drinkup_production; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE drinkup_production TO vagrant;" | sudo -u postgres psql'
+
+#add default cookbook_file
+cookbook_file "nginx-default" do
+	path "/etc/nginx/sites-available/default"
+end
+
+execute "nginx_restart" do 
+	command "service nginx restart"
+end
+
+#Rails setup
+package "ruby-dev"
+package "zlib1g-dev"
+package "nodejs"
+
+execute 'bundler install' do
+	command 'gem install bundler --conservative'
+end
+
+execute 'bundle' do
+	command 'bundle install'
+	cwd 'home/vagrant/project/drinkup'
+	user 'vagrant'
+end
+
+execute 'migrate' do
+	command 'rake db:migrate RAILS_ENV=production'
+	cwd 'home/vagrant/project/drinkup'
+	user 'vagrant'
+end
+
+execute 'precompile_assets' do
+	command 'RAILS_ENV=production bundle exec rake assets:precompile'
+	cwd 'home/vagrant/project/drinkup'
+	user 'vagrant'
+end
+
+#setting up unicorn stuff
+cookbook_file "unicorn_rails" do
+	path "/etc/init.d/unicorn_rails"
+end
+
+execute "permissions" do 
+	command "chmod +x /etc/init.d/unicorn_rails"
+end
+
+execute 'unicorn' do
+	command 'update-rc.d unicorn_rails defaults'
+end
+
+execute 'startup' do
+	command 'service unicorn_rails start'
+end

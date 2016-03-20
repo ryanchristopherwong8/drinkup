24fcd702402ddfbca8ec24510a91abd4390d9701
gargirou <gargirou@sfu.ca>
2016-03-20 16:06:44 -0700

    Added validations on event creation

--- drinkup/app/models/event.rb
+++ drinkup/app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
-
 	has_many :attendees
+	validates :name, presence: true
+	validates :lat, presence: true
+	validates :lng, presence: true
+	validates :gender, presence: true, inclusion: { in: %w(male female),
+    		 				message: "%{value} is not a valid gender" }
+    validates :datetime, presence: true
 end

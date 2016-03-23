25183bfa8b4cbbe9dc3b252a7971d9c80ed598a0
Nikhil Gaba <nga11@sfu.ca>
2016-03-22 19:37:37 -0700

    Revamped map logic on Create Event page

--- drinkup/app/assets/javascripts/GoogleAPI.js
+++ drinkup/app/assets/javascripts/GoogleAPI.js
@@ -1,52 +1,84 @@
-var user_location;
-var drinktype='bar';
-var radius_distance=1000;
+var map;
+var autocomplete;
+var service;
 var postionOfUserFromGeolocation=new Array;
+var drinktype='cafe'
+var infowindow;
 
 function getUserLocation()
 {
 	if (navigator.geolocation) 
 	{
-		console.log("nav");
 		var options={timeout:30000};
-        navigator.geolocation.getCurrentPosition(storePosition,showError,options);
+        navigator.geolocation.getCurrentPosition(storePosition,getManualLocation,options);
     } 
 }
 
 function storePosition(position) {
-	console.log("show store");
 	postionOfUserFromGeolocation[0]=position.coords.latitude;
 	postionOfUserFromGeolocation[1]=position.coords.longitude;
-	initMap();
+	changeMapLocation(postionOfUserFromGeolocation[0],postionOfUserFromGeolocation[1],15);
+  setup(postionOfUserFromGeolocation[0],postionOfUserFromGeolocation[1],1000,drinktype);
 }
 
-function showError(error) {
-	console.log("show error");
-	console.warn('ERROR(' + error.code + '): ' + error.message);
+function getManualLocation(error) {
+	//generate message that location could not be got
+  document.getElementById('error').innerHTML="Could Not Get Geolocation";
 }
 
-var map;
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+      {types: ['geocode']});
 
-      function initMap() {
-		console.log(postionOfUserFromGeolocation[0]);
-		console.log(postionOfUserFromGeolocation[1]);
-        var user_location = {lat:postionOfUserFromGeolocation[0] , lng:postionOfUserFromGeolocation[1]};
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', storePositionFromGoogleAPI);
+}
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: user_location,
-          zoom: 15
+function initMap(lat_user,lng_user,zoom) {
+  var user_location = {lat:lat_user , lng:lng_user};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: user_location,
+      zoom: zoom
         });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: user_location,
-          radius: radius_distance,
-          type: [drinktype]
-        }, callback);
+  service = new google.maps.places.PlacesService(map);
+  infowindow = new google.maps.InfoWindow();
       }
 
-      function callback(results, status) {
+function changeMapLocation(lat_user,lng_user,zoom)
+{
+	var user_location = {lat:lat_user , lng:lng_user};
+	map.setCenter(user_location);
+  map.setZoom(zoom);
+}
+
+function storePositionFromGoogleAPI()
+{
+	var place = autocomplete.getPlace();
+	postionOfUserFromGeolocation[0]=place.geometry.location.lat();
+	postionOfUserFromGeolocation[1]=place.geometry.location.lng();
+	changeMapLocation(postionOfUserFromGeolocation[0],postionOfUserFromGeolocation[1],15);
+  setup(postionOfUserFromGeolocation[0],postionOfUserFromGeolocation[1],1000, drinktype);
+}
+
+function setup(lat_user,lng_user,radius,drinktype)
+{
+  document.getElementById('error').innerHTML="";
+  var user_location = {lat:lat_user , lng:lng_user};
+  
+        
+        service.nearbySearch({
+          location: user_location,
+          radius: radius,
+          type: [drinktype]
+        }, callback);
+}
+
+function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
@@ -54,7 +86,7 @@ var map;
         }
       }
 
-      function createMarker(place) {
+function createMarker(place) {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
           map: map,
@@ -66,4 +98,3 @@ var map;
           infowindow.open(map, this);
         });
       }
-

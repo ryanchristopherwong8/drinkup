9ec85982a1dadf293eb47b2fa505279e38f7f1f2
ffa7 <ffa7@sfu.ca>
2016-04-04 19:07:26 -0700

    fixed loading of javascript unique to certain views

--- drinkup/config/initializers/assets.rb
+++ drinkup/config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.precompile += %w( react.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( conversations.js )
+Rails.application.config.assets.precompile += %w( listRenderer.js )
+Rails.application.config.assets.precompile += %w( eventRenderer.js )
+Rails.application.config.assets.precompile += %w( chat.js )

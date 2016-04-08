318415e058f43bb7fdde70bd37d5ed13cbb2f7ca
ffa7 <ffa7@sfu.ca>
2016-04-07 19:02:03 -0700

    fixed spelling error

--- drinkup/config/application.rb
+++ drinkup/config/application.rb
@@ -23,6 +23,6 @@ module Drinkup
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.schema_format = :sql
     config.active_record.raise_in_transactional_callbacks = true
-    cconfig.active_record.default_timezone = :utc
+    config.active_record.default_timezone = :utc
   end
 end

476c29e328dfcaaac4560220bc84bb99c5f68d48
ffa7 <ffa7@sfu.ca>
2016-04-11 15:42:37 -0700

    changed footer, added some styling for show button and conversation color

--- drinkup/app/assets/javascripts/listRenderer.js
+++ drinkup/app/assets/javascripts/listRenderer.js
@@ -6,10 +6,10 @@ function renderListWithPhotos(results,page) {
     if (page=="createPage")
     {
       $("#resultsList").css("display","none");
-      locationListItem.setAttribute("class", "list-group-item location listItem listitem_hover");
+      locationListItem.setAttribute("class", "list-group-item location listItem listitem_hover event_links");
     }else{
       $("#My-Events-Title").show();
-      locationListItem.setAttribute("class", "list-group-item location listItem");
+      locationListItem.setAttribute("class", "list-group-item location listItem event_links");
     }
     
 

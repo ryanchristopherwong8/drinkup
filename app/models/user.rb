020d998497f5fdbb0be0068f3c1ede0ef9ebbc04
Ryan Wong <ryancwong8@gmail.com>
2016-05-17 03:21:31 -0700

    Updates paperclip to use https

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :conversations
 
   #profile pictures
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :s3_protocol => :https
   # Validate content type
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   # Validate filename

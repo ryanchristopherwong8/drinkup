c69f5342d8a0db0dd74d3ba15bf473ccb710c8a1
Farzin Faghihi <farzin@Farzins-MacBook-Pro.local>
2017-04-01 20:27:27 -0700

    fixed aws region environment variable name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: ENV['AWS_S3_REGION']
+      s3_region: ENV['AWS_REGION']
     }
 
   }
